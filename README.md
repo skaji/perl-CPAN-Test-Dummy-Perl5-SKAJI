4005f51bdc3788713d05f1737474e5b49e1815dc
Shoichi Kaji <skaji@cpan.org>
2026-05-09 10:58:21 +0900

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+[![Actions Status](https://github.com/skaji/perl-CPAN-Test-Dummy-Perl5-SKAJI/actions/workflows/test.yml/badge.svg)](https://github.com/skaji/perl-CPAN-Test-Dummy-Perl5-SKAJI/actions)
+
+# NAME
+
+CPAN::Test::Dummy::Perl5::SKAJI - a test dummy distribution
+
+# SYNOPSIS
+
+    use CPAN::Test::Dummy::Perl5::SKAJI;
+
+# DESCRIPTION
+
+CPAN::Test::Dummy::Perl5::SKAJI is a test dummy distribution.
+
+# COPYRIGHT AND LICENSE
+
+Copyright 2026 Shoichi Kaji <skaji@cpan.org>
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
---
+++ dist.pl
@@ -0,0 +1,41 @@
+my @prereq = (
+    [ Prereqs => 'ConfigureRequires' ] => [
+        'Module::Build::Tiny' => '0.051',
+        'perl' => 'v5.24',
+    ],
+    [ Prereqs => 'RuntimeRequires' ] => [
+        'perl' => 'v5.24',
+    ],
+);
+
+my @plugin = (
+    'ExecDir' => [ dir => 'script' ],
+    'Git::GatherDir' => [ exclude_filename => 'META.json' ],
+    'CopyFilesFromBuild' => [ copy => 'META.json' ],
+    'VersionFromMainModule' => [],
+    'ReversionOnRelease' => [ prompt => 1 ],
+    'NextRelease' => [ format => '%v  %{yyyy-MM-dd HH:mm:ss VVV}d%{ (TRIAL RELEASE)}T' ],
+    'Git::Check' => [ allow_dirty => 'Changes', allow_dirty => 'META.json' ],
+    'GithubMeta' => [ issues => 1 ],
+    'ReadmeAnyFromPod' => [ type => 'markdown', filename => 'README.md', location => 'root' ],
+    'MetaProvides::Package' => [ inherit_version => 0, inherit_missing => 0 ],
+    'PruneFiles' => [ filename => 'dist.pl', filename => 'README.md', match => '^(xt|author|maint|example|eg)/' ],
+    'GitHubREADME::Badge' => [ badges => 'github_actions/test.yml' ],
+    'GenerateFile' => [ filename => 'Build.PL', content => "use Module::Build::Tiny;\nBuild_PL();" ],
+    'MetaJSON' => [],
+    'Metadata' => [ x_static_install => 1 ],
+    'Git::Contributors' => [],
+
+    'CheckChangesHasContent' => [],
+    'ConfirmRelease' => [],
+    'UploadToCPAN' => [],
+    'CopyFilesFromRelease' => [ match => '\.pm$' ],
+    'Git::Commit' => [ commit_msg => '%v', allow_dirty => 'Changes', allow_dirty => 'META.json', allow_dirty_match => '\.pm$' ],
+    'Git::Tag' => [ tag_format => '%v', tag_message => '%v' ],
+    'Git::Push' => [],
+);
+
+my @config = (
+    name => 'CPAN-Test-Dummy-Perl5-SKAJI',
+    [ @prereq, @plugin ],
+);
